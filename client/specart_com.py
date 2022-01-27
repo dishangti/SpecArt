9dc6f9af269deb571b2d2d7fa5857f030ec72bec
dishangti <16698219+dishangti@users.noreply.github.com>
2022-01-27 17:04:31 +0800

    Fix bugs in init resources.

--- client/specart_com.py
+++ client/specart_com.py
@@ -3,7 +3,7 @@ import socket
 import threading
 
 class Player(object):
-    def __init__(self, username, initGoods, initMoney, soc:socket.socket):
+    def __init__(self, username):
         '''
         username:用户名
         initGoods:初始物资
@@ -12,9 +12,8 @@ class Player(object):
         '''
 
         self.username = username
-        self.goods = initGoods
-        self.money = initMoney
-        self.soc = soc
+        self.goods = 0
+        self.money = 0
         self.transaction = {}       #keys:挂单时间 values:指令按空格切分后的列表
 
     # def sendMessage(self, port):
@@ -28,6 +27,59 @@ class Player(object):
     #     soc.connect((host, port))
     #     print(soc.recv(1024))
     #     soc.close()
+
+class Com:
+    def __init__(self, mode, username):
+        '''
+        mode (integer): 0 (consle), 1 (GUI)
+        '''
+        self.mode = mode
+        self.soc = socket.socket()
+        self.player = Player(username)
+        self.initGoods = 0
+        self.initMoney = 0
+
+    def connect(self, host, port = 7733):
+        self.soc.connect((host, port))
+        thread = threading.Thread(target=self.soc_recv, name='socRecvThread')
+        thread.start()
+        self.soc.sendall(f'name {self.player.username}#'.encode('utf8'))
+
+    def output(self, content):
+        if self.mode == 0:
+            # Console mode
+            self.CON_display(content)
+        elif self.mode == 1:
+            self.GUI_fresh()
+
+    def CON_display(self, content):
+        pass
+
+    def GUI_fresh(self):     # 于GUI中实现显示
+        pass
+
+    def send_cmd(self, cmd):
+        if type(cmd) == str:
+            cmd = cmd.strip().split()
+        if cmd[0] == 'sell':
+            self.player.sell(int(cmd[1]), int(cmd[2]))
+        elif cmd[0] == 'buy':
+            self.player.buy(int(cmd[1]), int(cmd[2]))
+        elif cmd[0] == 'backsell':
+            self.player.backsell(cmd[1], cmd[2], cmd[3])
+        elif cmd[0] == 'backbuy':
+            self.player.backbuy(cmd[1], cmd[2], cmd[3])
+
+    def soc_recv(self):
+        while True:
+            buff = self.player.soc.recv(1024).decode('utf8')
+            command = buff.split('#')
+            for i in command[:]:                    #去除空指令
+                if not i:
+                    command.remove(i)
+            for item in command:
+                out = item.strip().split()
+                self.output(out)
 
     def sell(self, num, price):
         '''
@@ -68,54 +120,3 @@ class Player(object):
 
         command = f'backbuy {num} {price} {time}#'
         self.soc.sendall(command.encode('utf8'))
-
-class Com:
-    def __init__(self, mode, player:Player):
-        '''
-        mode (integer): 0 (consle), 1 (GUI)
-        '''
-        self.mode = mode
-        self.soc = socket.socket()
-        self.player = player
-
-    def connect(self, host, port = 7733):
-        self.player.soc.connect((host, port))
-        thread = threading.Thread(target=self.soc_recv, name='socRecvThread')
-        thread.start()
-        self.soc.sendall(f'name {self.player.username}#'.encode('utf8'))
-      
-    def output(self, content):
-        if self.mode == 0:
-            # Console mode
-            self.CON_fresh(content)
-        elif self.mode == 1:
-            self.GUI_display(content)
-
-    def CON_fresh(self, content):
-        pass
-
-    def GUI_fresh(self, content):     # 于GUI中实现显示
-        pass
-
-    def send_cmd(self, cmd):
-        if type(cmd) == str:
-            cmd = cmd.strip().split()
-        if cmd[0] == 'sell':
-            self.player.sell(int(cmd[1]), int(cmd[2]))
-        elif cmd[0] == 'buy':
-            self.player.buy(int(cmd[1]), int(cmd[2]))
-        elif cmd[0] == 'backsell':
-            self.player.backsell(cmd[1], cmd[2], cmd[3])
-        elif cmd[0] == 'backbuy':
-            self.player.backbuy(cmd[1], cmd[2], cmd[3])
-
-    def soc_recv(self):
-        while True:
-            buff = self.player.soc.recv(1024).decode('utf8')
-            command = buff.split('#')
-            for i in command[:]:                    #去除空指令
-                if not i:
-                    command.remove(i)
-            for item in command:
-                out = item.strip().split()
-                self.output(out)
