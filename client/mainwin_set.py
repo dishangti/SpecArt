bb93405e596aa858b6032be7502192121c90b803
dishangti <16698219+dishangti@users.noreply.github.com>
2022-02-01 19:54:47 +0800

    Use slot & signal to fix multithread problems.

--- client/mainwin_set.py
+++ client/mainwin_set.py
@@ -1,4 +1,3 @@
-import queue
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
 from PyQt5.QtGui import QColor, QBrush
 from PyQt5.QtCore import pyqtSignal
@@ -15,40 +14,48 @@ class mainCom(Com):
         return super().__init__(mode)
 
     def GUI_fresh(self):
-        # Fresh winning process
-        if self.totalPlayerMoney != 0:
-            self.window.win_progressBar.value = int(self.player.money / self.totalPlayerMoney)
-        
-        # Fresh waiting order list
-        table = self.window.sell_tableWidget
-        table.setRowCount(0)
-        table.clearContents()
-        for item in self.buying.items:
-            row = table.rowCount()
-            table.insertRow(row)
-            table.setItem(row, 0, str(item[0]))     # Fill in price and number
-            table.setItem(row, 1, str(item[1]))
-            table.item(row, 0).setForeground(QBrush(QColor(0, 255, 0)))     # Set green color
-            table.item(row, 1).setForeground(QBrush(QColor(0, 255, 0)))
-            
-        table = self.window.buy_tableWidget
-        table.setRowCount(0)
-        table.clearContents()
-        for item in self.selling.items:
-            row = table.rowCount()
-            table.insertRow(row)
-            table.setItem(row, 0, str(item[0]))     # Fill in price and number
-            table.setItem(row, 1, str(item[1]))
-            table.item(row, 0).setForeground(QBrush(QColor(255, 0, 0)))     # Set red color
-            table.item(row, 1).setForeground(QBrush(QColor(255, 0, 0)))
-
-        # Fresh goods and money in status bar
-        self.window.statusbar.showMessage(f'状态 # 金钱: {self.player.money} | 物资: {self.player.goods}')
-
-        # Fresh price in LCD
-        self.window.price_lcdNumber.intValue = self.price
+        self.window.fresh_GUI()
 
     def GUI_newDeal(self, dir, price, num):
+        self.window.new_deal(dir, price, num)
+
+    def GUI_msgbox(self, content):
+        self.window.new_notice(content)
+
+class mainWin(Ui_SpecArt_MainWindow, QMainWindow):
+    newNotice = pyqtSignal()
+    newData = pyqtSignal()
+    newDeal = pyqtSignal()
+
+    def __init__(self):
+        super(Ui_SpecArt_MainWindow, self).__init__()
+        self.com = mainCom(1, self)
+        self.setupUi(self)
+
+        self.notice_que = Queue()
+        self.deal_que = Queue()
+
+        # Set QTableWidget uneditable
+        self.sell_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.buy_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.deal_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.com.notice('登录成功！等待服务器开始游戏...')
+
+        # Set slots for signals
+        self.newNotice.connect(self.display_notice)
+        self.newData.connect(self.fresh_GUI)
+        self.newDeal.connect()
+
+    # def currentPriceShow(self):
+    #     self.price_lcdNumber.setDigitCount(len(self.price))
+    #     self.price_lcdNumber.display(self.price)
+
+    def display_notice(self):
+        if not self.notice_que.empty():
+            msg = self.notice_que.get()
+            QMessageBox.information(self.window, '提示', msg)
+    
+    def add_deal_item(self, dir, price, num):
         # Red for positive buy (0)
         # Green for positive sell (1)
         table = self.window.deal_tableWidget
@@ -67,38 +74,55 @@ class mainCom(Com):
             table.item(row, 0).setForeground(QBrush(QColor(0, 255, 0)))
             table.item(row, 1).setForeground(QBrush(QColor(0, 255, 0)))
 
-    def GUI_msgbox(self, content):
-        self.window.new_notice(content)
+    def display_deal(self):
+        if not self.deal_que.empty():
+            dir, price, num = self.deal_que.get()
+            self.add_deal_item(dir, price, num)
 
-class mainWin(Ui_SpecArt_MainWindow, QMainWindow):
-    newNotice = pyqtSignal()
+    def fresh_GUI(self):
+        # Fresh winning process
+        if self.com.totalPlayerMoney != 0:
+            self.win_progressBar.value = int(self.com.player.money / self.com.totalPlayerMoney)
+        
+        # Fresh waiting order list
+        table = self.sell_tableWidget
+        table.setRowCount(0)
+        table.clearContents()
+        for item in self.com.buying.items:
+            row = table.rowCount()
+            table.insertRow(row)
+            table.setItem(row, 0, str(item[0]))     # Fill in price and number
+            table.setItem(row, 1, str(item[1]))
+            table.item(row, 0).setForeground(QBrush(QColor(0, 255, 0)))     # Set green color
+            table.item(row, 1).setForeground(QBrush(QColor(0, 255, 0)))
+            
+        table = self.buy_tableWidget
+        table.setRowCount(0)
+        table.clearContents()
+        for item in self.com.selling.items:
+            row = table.rowCount()
+            table.insertRow(row)
+            table.setItem(row, 0, str(item[0]))     # Fill in price and number
+            table.setItem(row, 1, str(item[1]))
+            table.item(row, 0).setForeground(QBrush(QColor(255, 0, 0)))     # Set red color
+            table.item(row, 1).setForeground(QBrush(QColor(255, 0, 0)))
 
-    def __init__(self):
-        super(mainWin, self).__init__()
-        self.com = mainCom(1, self)
-        self.setupUi(self)
+        # Fresh goods and money in status bar
+        self.statusbar.showMessage(f'状态 # 金钱: {self.player.money} | 物资: {self.player.goods}')
 
-        self.notice_que = Queue()
+        # Fresh price in LCD
+        self.price_lcdNumber.intValue = self.price
 
-        # Set QTableWidget uneditable
-        self.sell_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.buy_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.deal_tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-        self.com.notice('登录成功！等待服务器开始游戏...')
-
-    def currentPriceShow(self):
-        self.lcdNumber.setDigitCount(len(self.price))
-        self.lcdNumber.display(self.price)
-
-    def display_notice(self):
-        if not self.notice_que.empty():
-            msg = self.notice_que.get()
-            QMessageBox.information(self.window, '提示', msg)
-    
     def new_notice(self, content):
         self.notice_que.put(content)
         self.newNotice.emit()
 
+    def new_deal(self, dir, price, num):
+        self.deal_que.put((dir, price, num))
+        self.newDeal.emit()    
+
+    def new_data(self):
+        self.newData.emit()
 
 if __name__ == '__main__':
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
